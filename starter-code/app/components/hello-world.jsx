45cf01c261b8ed9b9382f177308757917557711e
Ilias Tsangaris <ilias@ga.co>
2016-07-11 21:37:01 -0700

    Update hello-world.jsx

--- starter-code/app/components/hello-world.jsx
+++ starter-code/app/components/hello-world.jsx
@@ -1,12 +1,1 @@
-import React from 'react'
-
-export default class HelloWorld extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>Hello {this.props.name}</p>
-        <p>You are {this.props.mood}!</p>
-      </div>
-    )
-  }
-}
+// hello world component...
